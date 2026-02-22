1e32f295b8db3e316e97b98aec8f16a679df239d
Seiryo Pramanda <seiryobayu@gmail.com>
2026-02-23 06:13:25 +0700

    feat: Implement user service CLI and configuration management

---
+++ user-service/config/config.go
@@ -0,0 +1,77 @@
+package config
+
+import "github.com/spf13/viper"
+
+type App struct {
+	AppPort string `json:"app_port"`
+	AppEnv  string `json:"app_env"`
+}
+
+type SqlDB struct {
+	Host           string `json:"host"`
+	Port           string `json:"port"`
+	User           string `json:"user"`
+	Password       string `json:"password"`
+	DBName         string `json:"db_name"`
+	DBMaxOpenConns string `json:"db_max_open_connections"`
+	DBMaxIdleConns string `json:"db_max_idle_connections"`
+}
+
+type RabbitMQ struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+}
+
+type Redis struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+}
+
+type Supabase struct {
+	Url    string `json:"url"`
+	Key    string `json:"key"`
+	Bucket string `json:"bucket"`
+}
+
+type Config struct {
+	App      App      `json:"app"`
+	SqlDB    SqlDB    `json:"sql_db"`
+	RabbitMQ RabbitMQ `json:"rabbitmq"`
+	Redis    Redis    `json:"redis"`
+	Supabase Supabase `json:"supabase"`
+}
+
+func NewConfig() *Config {
+	return &Config{
+		App: App{
+			AppPort: viper.GetString("APP_PORT"),
+			AppEnv:  viper.GetString("APP_ENV"),
+		},
+		SqlDB: SqlDB{
+			Host:           viper.GetString("DATABASE_HOST"),
+			Port:           viper.GetString("DATABASE_PORT"),
+			User:           viper.GetString("DATABASE_USER"),
+			Password:       viper.GetString("DATABASE_PASSWORD"),
+			DBName:         viper.GetString("DATABASE_NAME"),
+			DBMaxOpenConns: viper.GetString("DATABASE_MAX_OPEN_CONNECTION"),
+			DBMaxIdleConns: viper.GetString("DATABASE_MAX_IDLE_CONNECTION"),
+		},
+		RabbitMQ: RabbitMQ{
+			Host:     viper.GetString("RABBITMQ_HOST"),
+			Port:     viper.GetString("RABBITMQ_PORT"),
+			User:     viper.GetString("RABBITMQ_USER"),
+			Password: viper.GetString("RABBITMQ_PASSWORD"),
+		},
+		Redis: Redis{
+			Host: viper.GetString("REDIS_HOST"),
+			Port: viper.GetString("REDIS_PORT"),
+		},
+		Supabase: Supabase{
+			Url:    viper.GetString("SUPABASE_URL"),
+			Key:    viper.GetString("SUPABASE_KEY"),
+			Bucket: viper.GetString("SUPABASE_BUCKET"),
+		},
+	}
+}
---
+++ user-service/models/role_model.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+type Role struct {
+	ID        uint      `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
